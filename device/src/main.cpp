0209070fe9a25cc0888038dd734b090d5bf69989
yosshipaopao <yosshipaopao@gmail.com>
2024-07-12 15:49:22 +0900

    add device code

---
+++ device/src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include "Keyboard.h"
+
+#define rep(i,n) for(int i=0;i<n;i++)
+
+const int cnt = 8;
+const int pins[] = {2,3,4,5,9,11,13,14};
+const char keys[] = {'w','s','a','d','i','j','k','l'};;
+
+void setup() {
+  Keyboard.begin();
+  rep(i,cnt)pinMode(pins[i],INPUT_PULLUP);
+}
+
+void loop() {
+  rep(i,cnt){
+    if(digitalRead(pins[i]) == LOW) Keyboard.press(keys[i]);
+    else Keyboard.release(keys[i]);
+  }
+}
