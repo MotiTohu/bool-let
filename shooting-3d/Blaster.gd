d2eb568caa20c7b3f1bf43d4fb739071670f72f7
yosshipaopao <87005177+yosshipaopao@users.noreply.github.com>
2024-07-19 21:38:26 +0900

    fix graphic

--- shooting-3d/Blaster.gd
+++ shooting-3d/Blaster.gd
@@ -1,9 +1,17 @@
 extends Sprite3D
 
-@export var follow_weight := 10.0
-@export var z_offset := 0.01
+@export var follow_weight := 30.0
+@export var z_offset := 0.02
+@export_category("float")
+@export var float_speed := 1.0
+@export var float_weight := 50.0
 
+var delta_sum := 0.0
 func _process(delta):
-	rotation = rotation.slerp(get_parent().rotation, delta * follow_weight)
+	rotation.z = lerpf(rotation.z,get_parent().rotation.z,delta * follow_weight)
 	global_position = global_position.slerp(get_parent().global_position,delta * follow_weight)
 	global_position.z += z_offset
+	
+	delta_sum += delta * float_speed
+	delta_sum = fmod(delta_sum,PI*2)
+	offset.y = sin(delta_sum) * float_weight
--- shooting-3d/CharacterBody3D.gd
+++ shooting-3d/CharacterBody3D.gd
@@ -1,10 +1,11 @@
 extends CharacterBody3D
 
 
-const SPEED := 300.0
-const DEPTH_SPEED := 3.0
-const ROTATE_SPEED := 10.0
-@onready var sprite_3d := $Node3D/Sprite3D
+@export var SPEED := 600.0
+@export var DEPTH_SPEED := 3.0
+@export var ROTATE_SPEED := 10.0
+@export var MOVE_DEPTH := 10.0
+
 @onready var node_3d = $Node3D
 @onready var collision_shape_3d = $CollisionShape3D
 @onready var aim = $Aim
@@ -24,10 +25,10 @@ func _move(delta:float)->void:
 	else:
 		velocity.x = move_toward(velocity.x,0,SPEED * delta)
 		velocity.y = move_toward(velocity.y,0,SPEED * delta)
-	velocity.z = (input_depth - position.z) * DEPTH_SPEED * (2.0 if input_depth else 1.0)
+	velocity.z = (input_depth * MOVE_DEPTH - position.z) * DEPTH_SPEED * (2.0 if input_depth else 1.0)
 	move_and_slide()
-	position.x = clampf(position.x,-4,4)
-	position.y = clampf(position.y,-3,3)
+	position.x = clampf(position.x,-8,8)
+	position.y = clampf(position.y,-6,6)
 
 func _rotate(delta:float)->void:
 	var rotate_to := -Input.get_axis("left","right") * PI / 8
