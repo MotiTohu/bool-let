508b107f7ca9fd38a4da7d1defa33654cf7a18a8
yosshipaopao <yosshipaopao@gmail.com>
2024-07-30 14:00:04 +0900

    update

--- shooting-3d/scenes/Charactors/Enemy/ChildEnemy.gd
+++ shooting-3d/scenes/Charactors/Enemy/ChildEnemy.gd
@@ -1,15 +1,11 @@
 extends Enemy
 class_name ChildEnemy 
 
-
-
-func _on_timer_timeout() -> void:
-	var bullet := ENEMY_BULLET.instantiate()
-	add_child(bullet)
-
 var hp := 10.0
 func decrease_hp(diff_value:float)->void:
 	hp -= diff_value
 	if hp <= 0.0:
 		queue_free()
-	
+
+func _on_child_timeout():
+	return
