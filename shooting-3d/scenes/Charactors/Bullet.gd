934b3ab3e9a0fd88fb0b0c51da7f862c0f755772
MotiTohu <goonyan285@gmail.com>
2024-10-13 08:39:03 +0900

    up form

--- shooting-3d/scenes/Charactors/Bullet.gd
+++ shooting-3d/scenes/Charactors/Bullet.gd
@@ -6,6 +6,7 @@ class_name Bullet extends Area3D
 @export var DAMAGE := 1.0
 
 const EFFECT := preload("res://scenes/Graphics/effect_playerhit.tscn")
+const EXPLOSION = preload("res://scenes/Graphics/effect_explosion1.tscn")
 
 func _ready() -> void:
 	MOVE_TO = MOVE_TO.normalized()
@@ -28,6 +29,15 @@ func _on_body_entered(body: Node3D) -> void:
 		if body is not Enemy:
 			if ! body.damaga_able:
 				queue_free()
+			for i in range(3):
+				var explode := EXPLOSION.instantiate()
+				body.add_child(explode)
+				explode.global_position = global_position
+				explode.global_position = body.global_position+Vector3(randf_range(-0.7, 0.7),randf_range(-0.5, 0.5),randf_range(0.01, 0.2))
+				explode.rotation.x = randf_range(0,2.0)*PI
+				explode.rotation.y = randf_range(0,2.0)*PI
+				explode.rotation.z = randf_range(0,2.0)*PI
+				
 		body.decrease_hp(DAMAGE)
 		if body is Enemy:
 			var effect := EFFECT.instantiate()
